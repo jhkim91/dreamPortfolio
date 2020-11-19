dc13061411b8b3779078abe115dac6b5cf691890
Park Leslie <kinterch@gmail.com>
2020-11-19 09:31:35 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-'use strict'
+"use strict";
 
 // Make navbar transparent when it is on the top
 // MDN: Window.scrollY: https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
 // MDN: Determining the dimensions of elements: https://developer.mozilla.org/en-US/docs/Web/API/CSS_Object_Model/Determining_the_dimensions_of_elements
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-  if(window.scrollY > navbarHeight){
-    navbar.classList.add('navbar--dark')
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
   } else {
-    navbar.classList.remove('navbar--dark')
+    navbar.classList.remove("navbar--dark");
   }
-})
+});
 
+// Handle scrolling when tapping on the navbar menu
+// MDN: Element.scrollIntoView: https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  const link = homeContactBtn.dataset.link;
+  if (link == null) return;
+  scrollIntoView(link);
+});
+
+function scrollIntoView(selector) {
+  var scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
