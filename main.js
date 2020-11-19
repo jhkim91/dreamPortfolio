55eb9f48b5d771efac4c62b3a5158cc37ecf7f4c
Park Leslie <kinterch@gmail.com>
2020-11-19 17:24:22 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -33,6 +33,13 @@ homeContactBtn.addEventListener("click", () => {
   scrollIntoView(link);
 });
 
+// Mmake home slowly fade to transparent as the window scrolls down.home__contact
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
   var scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
