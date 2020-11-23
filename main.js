3055d51e1474814333cf36da9868a9af8c48546a
Park Leslie <kinterch@gmail.com>
2020-11-24 08:35:55 +0900

    JS: Change menu selection when scrolling

--- main.js
+++ main.js
@@ -75,7 +75,6 @@ workBtnContainer.addEventListener("click", (e) => {
   active.classList.remove("selected");
   const target =
     e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
-  console.log("target", target, e.target.nodeName);
   target.classList.add("selected");
 
   projectContainer.classList.add("anim-out");
@@ -93,7 +92,76 @@ workBtnContainer.addEventListener("click", (e) => {
   }, 300);
 });
 
+// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+const sectionIds = [
+  "#home",
+  "#about",
+  "#skills",
+  "#work",
+  "#testimonials",
+  "#contact",
+];
+
+const sections = sectionIds.map((id) => document.querySelector(`${id}`));
+
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+
+function selectNavItem(selected) {
+  // if (selectedNavItem.className.includes("active"))
+  selectedNavItem.classList.remove("active");
+  selectedNavItem = selected;
+  selectedNavItem.classList.add("active");
+}
+
 function scrollIntoView(selector) {
   var scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+const observerOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
+
+window.addEventListener("wheel", () => {
+  // scroll: 브라우저에서 움직이는것(메뉴클릭시 스크롤되는 현상)도 다 포함, wheel: 사용자가 움직이는것만 포함
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    // window.scrollY: 스크롤 크기, window.innerHeight: 현재 보여지는 창높이
+    window.scrollY + window.innerHeight ===
+    document.body.clientHeight // body 크기
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
